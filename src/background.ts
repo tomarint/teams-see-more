0f2cf7845ce7b86716e0efc75f1d7e3ccf762bbd
tomarin <tomarint@gmail.com>
2023-05-28 02:58:34 +0900

    Create context menu on startup also Suppress an error log at chrome.contextMenus.remove Introduce logError

--- src/background.ts
+++ src/background.ts
@@ -1,26 +1,45 @@
-const contextMenuId = "teams-see-more-context-menu-id";
-const messageName = "teams-see-more-message";
+const contextMenuId: string = "teams-see-more-context-menu-id";
+const messageName: string = "teams-see-more-message";
+const successMessage: string = "success";
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create(
-    {
-      id: contextMenuId,
-      title: chrome.i18n.getMessage("extName"),
-      contexts: ["all"],
-      documentUrlPatterns: ["https://teams.microsoft.com/*"],
-    },
-    () => {
-      if (chrome.runtime.lastError) {
-        console.log(chrome.runtime.lastError);
-      }
+function logError(location: string): void {
+  if (chrome.runtime.lastError) {
+    console.log(`Error in ${location}: ${chrome.runtime.lastError.message}`);
+  }
+}
+
+function createContextMenu(): void {
+  chrome.contextMenus.remove(contextMenuId, () => {
+    if (chrome.runtime.lastError) {
+      // This is expected if the context menu has not yet been created.
+      // logError("contextMenus.remove");
     }
-  );
+    chrome.contextMenus.create(
+      {
+        id: contextMenuId,
+        title: chrome.i18n.getMessage("extName"),
+        contexts: ["all"],
+        documentUrlPatterns: ["https://teams.microsoft.com/*"],
+      },
+      () => {
+        logError("contextMenus.create");
+      }
+    );
+  });
+}
+
+chrome.runtime.onInstalled.addListener(createContextMenu);
+chrome.runtime.onStartup.addListener(createContextMenu);
+
+chrome.runtime.onUpdateAvailable.addListener((details: chrome.runtime.UpdateAvailableDetails) => {
+  console.log("updating to version " + details.version);
+  chrome.runtime.reload();
 });
 
 // Fired when a context menu item is clicked.
 chrome.contextMenus.onClicked.addListener(
   (info: chrome.contextMenus.OnClickData, tab?: chrome.tabs.Tab) => {
-    // console.log('context menu clicked');
+    // console.log("context menu clicked");
     // console.log(info);
     // console.log(tab);
     if (tab == null || tab.id == null || tab.id < 0) {
@@ -32,17 +51,13 @@ chrome.contextMenus.onClicked.addListener(
         {
           message: messageName,
         },
-        (response) => {
-          if (chrome.runtime.lastError) {
-            console.log(chrome.runtime.lastError);
-            return;
-          }
-          if (response != null && response.message === "success") {
-            // console.log("contextMenu is succeeded.");
+        (response: any) => { // Depending on your response, you may want to replace "any" with the actual type
+          logError("tabs.sendMessage");
+          if (response != null && response.message === successMessage) {
+            // console.log("contextMenu operation succeeded.");
           }
         }
       );
-      return true;
     }
   }
 );
